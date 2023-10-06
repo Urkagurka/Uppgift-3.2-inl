2c1c07d54668e1cacbed74df502f01db228733cf
alve06001 <alve06001@E-PW0234LA>
2023-10-06 13:03:04 +0200

    Add project files.

---
+++ Uppgift 3.2 inl/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? Svara J för ja och N för nej");
+            string Gymnasiet = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder < 22 && Gymnasiet == "J") 
+            {
+                Console.WriteLine("Vi vill gärna anställa dig.");
+            }
+            else
+            {
+                Console.WriteLine("Vi vill inte anställa dig");
+            }
+            
+        }
+    }
+}
